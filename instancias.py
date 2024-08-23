9cdf99a32486104e22b4e36051e058563e402cf2
XiorelyH <Xiorelyh@gmail.com>
2024-08-22 20:47:34 -0400

    Update instancias.py

--- instancias.py
+++ instancias.py
@@ -1,7 +1,7 @@
 from te import Te
 
-te_1 = Te() # Crea una instancia de la clase Te
-te_2 = "Te" # Es una cadena de texto, no una instancia de la clase Te
+te_1 = Te.retorna_tiempo_y_recomendacion(1)
+te_2 = Te.retorna_precio(300)
 
 tipo_1 = type(te_1) # Obtiene el tipo de te_1, que será <class 'Te'>
 tipo_2 = type(te_2) # Obtiene el tipo de te_2, que será <class 'str'>
